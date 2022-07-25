b8bdc3af9eec7d8a44e8cf6bd6bbe8f6235e19b0
Hannah Akhtar <hannahaakhtar@gmail.com>
2022-07-25 14:48:25 +0100

    frontend folder renamed

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Fake BnB</title>
+        <meta name="description" content="Created by Hannah Akhtar" />
+        <link rel="icon" href="logo.png" />
+      </Head>    
+      <h1>Hello World</h1>
+      <h2>Test comment 123</h2>
+    </div>
+  )
+}
